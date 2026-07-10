1711fbd9d903e78b4e6e4e17466744f212363ac2
thxdmw <86361677+thxdmw@users.noreply.github.com>
2026-07-10 17:02:29 +0800

    fix(gamesave): 明确快照表自增主键策略

--- src/main/java/com/thx/module/gamesave/model/GameSnapshot.java
+++ src/main/java/com/thx/module/gamesave/model/GameSnapshot.java
@@ -1,5 +1,6 @@
 package com.thx.module.gamesave.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -13,7 +14,7 @@ import java.util.Date;
 @TableName("game_snapshot")
 public class GameSnapshot {
 
-    @TableId
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String snapshotId;
     private String userId;
