b2eb2c383eb7f67411198d3e6b18c0b50ef0c177
thxdmw <86361677+thxdmw@users.noreply.github.com>
2026-07-10 17:01:57 +0800

    fix(gamesave): 明确设备表自增主键策略

--- src/main/java/com/thx/module/gamesave/model/GameDevice.java
+++ src/main/java/com/thx/module/gamesave/model/GameDevice.java
@@ -1,5 +1,6 @@
 package com.thx.module.gamesave.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -16,7 +17,7 @@ import java.util.Date;
 @TableName("game_device")
 public class GameDevice {
 
-    @TableId
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String deviceId;
     private String userId;
