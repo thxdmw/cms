4a0eedb6c3ca913df2f5f9037513cf2eb442fa68
thxdmw <86361677+thxdmw@users.noreply.github.com>
2026-07-10 17:58:30 +0800

    feat(gamesave): 在快照提交结果中标记是否创建新版本

--- src/main/java/com/thx/module/gamesave/dto/SnapshotCommitResult.java
+++ src/main/java/com/thx/module/gamesave/dto/SnapshotCommitResult.java
@@ -3,7 +3,7 @@ package com.thx.module.gamesave.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-/** 快照提交成功结果。 */
+/** 快照提交结果；created=false 表示 Manifest 与当前 HEAD 完全一致，本次同步为幂等 no-op。 */
 @Data
 @AllArgsConstructor
 public class SnapshotCommitResult {
@@ -12,4 +12,5 @@ public class SnapshotCommitResult {
     private int fileCount;
     private long logicalSize;
     private int changedFileCount;
+    private boolean created;
 }
