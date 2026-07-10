5a775255dd445f15bf7f4b18a02e1fde91a385d0
thxdmw <86361677+thxdmw@users.noreply.github.com>
2026-07-10 17:04:53 +0800

    fix(gamesave): 明确同步 HEAD 表自增主键策略

--- src/main/java/com/thx/module/gamesave/model/GameSyncHead.java
+++ src/main/java/com/thx/module/gamesave/model/GameSyncHead.java
@@ -1,5 +1,6 @@
 package com.thx.module.gamesave.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -13,7 +14,7 @@ import java.util.Date;
 @TableName("game_sync_head")
 public class GameSyncHead {
 
-    @TableId
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String userId;
     private String gameId;
